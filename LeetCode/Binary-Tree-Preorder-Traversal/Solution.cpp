b01e7c8fce1770894f008dcf253c6edda35cac30
Arpan Dixit <dixit.arpan20@gmail.com>
2026-07-26 14:13:39 +0530

    Add solution for Binary Tree Preorder Traversal

--- LeetCode/Binary-Tree-Preorder-Traversal/Solution.cpp
+++ LeetCode/Binary-Tree-Preorder-Traversal/Solution.cpp
@@ -11,22 +11,35 @@
 11 */
 12class Solution {
 13public:
-14    
-15    vector<int> preorderTraversal(TreeNode* root) {
-16        vector<int>ans;
-17        if(root==NULL)
-18        return ans;
-19        stack<TreeNode*>a;
-20        a.push(root);
-21        while(!a.empty()){
-22            TreeNode* node=a.top();
-23            a.pop();
-24            if(node->right!=NULL)
-25            a.push(node->right);
-26            if(node->left!=NULL)
-27            a.push(node->left);
-28            ans.push_back(node->val);
-29        }
-30        return ans;
-31    }
-32};
+14    TreeNode* iop(TreeNode* root){
+15        TreeNode* p=root->left;
+16        while(p->right!=NULL && p->right!=root){
+17            p=p->right;
+18        }
+19        return p;
+20    }
+21    vector<int> preorderTraversal(TreeNode* root) {
+22        vector<int>ans;
+23        if(root==NULL)
+24        return ans;
+25        TreeNode* curr=root;
+26        while(curr!=NULL){
+27            if(curr->left==NULL){
+28                ans.push_back(curr->val);
+29                curr=curr->right;
+30            }
+31            else{
+32                TreeNode* ip=iop(curr);
+33                if(ip->right==NULL){
+34                    ip->right=curr;
+35                    ans.push_back(curr->val);
+36                    curr=curr->left;
+37                }else{
+38                    ip->right=NULL;
+39                    curr=curr->right;
+40                }
+41            }
+42        }
+43        return ans;
+44    }
+45};
