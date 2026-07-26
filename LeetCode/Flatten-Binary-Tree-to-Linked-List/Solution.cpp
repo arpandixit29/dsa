4a6e98f92fc638314a6b0a4dc41bf0856538fed2
Arpan Dixit <dixit.arpan20@gmail.com>
2026-07-26 12:15:22 +0530

    Add solution for Flatten Binary Tree to Linked List

--- LeetCode/Flatten-Binary-Tree-to-Linked-List/Solution.cpp
+++ LeetCode/Flatten-Binary-Tree-to-Linked-List/Solution.cpp
@@ -15,10 +15,22 @@
 15    void flatten(TreeNode* root) {
 16        if(root==NULL)
 17        return;
-18        flatten(root->right);
-19        flatten(root->left);
-20        root->left=NULL;
-21        root->right=nextright;
-22        nextright=root;
-23    }
-24};
+18        TreeNode* pre=NULL;
+19        stack<TreeNode*>st;
+20        st.push(root);
+21        while(!st.empty()){
+22            TreeNode* curr=st.top();
+23            st.pop();
+24            if(pre){
+25                pre->left=NULL;
+26                pre->right=curr;
+27            }
+28            if(curr->right)
+29            st.push(curr->right);
+30            if(curr->left)
+31            st.push(curr->left);
+32            pre=curr;
+33
+34        }
+35    }
+36};
