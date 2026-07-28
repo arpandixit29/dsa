9445e39862fe89ac123a01ee96670cc7f231289b
Arpan Dixit <dixit.arpan20@gmail.com>
2026-07-29 01:09:17 +0530

    Add solution for Binary Tree Right Side View

---
+++ LeetCode/Binary-Tree-Right-Side-View/Solution.cpp
@@ -0,0 +1,11 @@
+          1
+       /     \
+      2       3
+       \       \
+        5       4
+
+Level 1 → [1]        → take 1
+Level 2 → [2, 3]     → take 3
+Level 3 → [5, 4]     → take 4
+
+Right side view = [1, 3, 4]
